edd975bec6ab6fab96d79a15a7b1a03463620c0c
Solthodox <mrwojack@proton.me>
2023-10-03 17:39:32 +0200

    remove export

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,6 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   trailingSlash: false,
   swcMinify: true,
